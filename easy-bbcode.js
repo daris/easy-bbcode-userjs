0389eb474d4502d41dbd76f6386d1158e48693ee
daris <daris91@gmail.com>
2012-01-07 10:30:52 +0100

    Make it working with ticket comments too

--- easy-bbcode.js
+++ easy-bbcode.js
@@ -1,3 +1,12 @@
+// ==UserScript==
+// @name        Easy BBCode
+// @description Shows a bbcode bar
+// @include     https://fluxbb.org/*
+// @include     http://fluxbb.org/*
+// @include     http://localhost/*
+// @author      daris
+// ==/UserScript==
+
 var fluxbb_smilies = {
 	':)' : 'smile.png',
 	'=)' : 'smile.png',
@@ -19,13 +28,8 @@ var fluxbb_smilies = {
 	':cool:' : 'cool.png'
 };
 
-function insert_text(open, close)
+function insert_text(msgfield, open, close)
 {
-	if (document.getElementsByName('req_message'))
-		msgfield = document.getElementsByName('req_message')[0];
-	else
-		document.all.req_message;
-
 	// IE support
 	if (document.selection && document.selection.createRange)
 	{
@@ -40,7 +44,7 @@ function insert_text(open, close)
 		var startPos = msgfield.selectionStart;
 		var endPos = msgfield.selectionEnd;
 		var selText = msgfield.value.substring(startPos, endPos);
-		
+
 		msgfield.value = msgfield.value.substring(0, startPos) + open + selText + close + msgfield.value.substring(endPos, msgfield.value.length);
 		if (selText != '')
 		{
@@ -50,7 +54,7 @@ function insert_text(open, close)
 		else
 		{
 			msgfield.selectionStart = startPos + open.length;
-			msgfield.selectionEnd = msgfield.selectionStart;    
+			msgfield.selectionEnd = msgfield.selectionStart;
 		}
 	}
 
@@ -59,7 +63,7 @@ function insert_text(open, close)
 		msgfield.value += open + close;
 
 	msgfield.focus();
-	
+
 	return;
 }
 
@@ -80,16 +84,31 @@ function get_quote_text()
 		quote_text = document.getSelection();
 }
 
-function Quote(id)
+function Quote(form, field, id)
 {
+	var msgfield = document.getElementById(form).elements[field];
+
 	blockpost = document.getElementById('p' + id);
-	postleft = getElementsByClassName('postleft', blockpost)[0];
-	dt = postleft.getElementsByTagName('dt')[0];
-	a = dt.getElementsByTagName('a')[0];
-	user_name = a.text;
-	
-	postleft = getElementsByClassName('postright', blockpost)[0];
-	postmsg = getElementsByClassName('postmsg', postleft)[0];
+	postlefttags = getElementsByClassName('postleft', blockpost);
+	if (postlefttags.length > 0)
+	{
+		postleft = postlefttags[0];
+
+		dt = postleft.getElementsByTagName('dt')[0];
+		a = dt.getElementsByTagName('a')[0];
+
+		user_name = a.text;
+		postleft = getElementsByClassName('postright', blockpost)[0];
+		postmsg = getElementsByClassName('postmsg', postleft)[0];
+	}
+	else
+	{
+		item_info = getElementsByClassName('item-info', blockpost)[0];
+		user = getElementsByClassName('user', item_info)[0];
+		user_name = user.getElementsByTagName('a')[0].text;
+		postmsg = getElementsByClassName('bbcode-formatted', blockpost)[0];
+	}
+
 	message = postmsg.innerHTML;
 	message = message.replace(/<p class="postedit"><em>.*?<\/em><\/p>/g, '');
 
@@ -97,18 +116,25 @@ function Quote(id)
 		message = message.replace(new RegExp('<img src=".*?' + fluxbb_smilies[smile] + '" width="\\d+" height="\\d+" alt=".*?">', 'g'), smile);
 
 	message = message.replace(/<a href="(.*?)">(.*?)<\/a>/g, url_replace);
-	
+
 	message = message.replace(/<div class="quotebox"><cite>(.*?) (wrote|napisał):<\/cite><blockquote><div>/g, '[quote=$1]');
 	message = message.replace(/<div class="quotebox"><blockquote><div>/g, '[quote]');
 	message = message.replace(/<\/div><\/blockquote><\/div>/g, '[/quote]');
 	message = message.replace(/<div class="codebox"><pre><code>/g, '[code]');
 	message = message.replace(/<\/code><\/pre><\/div>/g, '[/code]');
-	
+
 	message = message.replace(/<strong>/g, '[b]');
 	message = message.replace(/<\/strong>/g, '[/b]');
 	message = message.replace(/<em>/g, '[i]');
 	message = message.replace(/<\/em>/g, '[/i]');
 	message = message.replace(/<span class="bbu">(.*?)<\/span>/g, '[u]$1[/u]');
+
+	// Lists
+	message = message.replace(/<ol class="decimal">/g, '[list=1]');
+	message = message.replace(/<ol.*>/g, '[list]');
+	message = message.replace(/<li>/g, '[*]');
+	message = message.replace(/<\/li>/g, '[/*]');
+	message = message.replace(/<\/ol>/g, '[/list]');
 
 	message = message.replace(/&lt;/g, "<");
 	message = message.replace(/&gt;/g, ">");
@@ -126,7 +152,7 @@ function Quote(id)
 	message = message.replace(new RegExp("[\\n\\t]+$", "g"), "");
 
 	startq = '[quote=' + user_name + ']' + (quote_text != '' ? quote_text : message) + '[/quote]';
-	insert_text(startq,'');
+	insert_text(msgfield, startq, '');
 }
 
 function url_replace()
@@ -137,38 +163,57 @@ function url_replace()
 		return '[url=' + arguments[1] + ']' + arguments[2] + '[/url]';
 }
 
-addEventListener('load', function (e)
+function show_bbcode_bar(form, msgfield, no_smilies, quote_field)
 {
-	if (document.getElementsByName('req_message').length > 0)
+	if (!document.getElementById(form))
+		return false;
+
+	if (!document.getElementById(form).elements[msgfield])
+		return false;
+
+	var html = '';
+	if (!no_smilies)
 	{
-		var html = '<div style="float: right; margin-right: 10px">';
+		html += '<div style="float: right; margin-right: 10px">';
 		for (s in fluxbb_smilies)
 			html += '<img onclick="insert_text(\'' + s + '\', \'\');" src="img/smilies/' + fluxbb_smilies[s] + '" width="15" height="15" style="margin-right: 3px" alt="' + s + '" title="' + s + '" />';
 		html += '</diV>';
-		html += '<div>';
-		html += '<input type="button" value="B" name="B" onclick="insert_text(\'[b]\',\'[/b]\')" /> ';
-		html += '<input type="button" value="I" name="I" onclick="insert_text(\'[i]\',\'[/i]\')" /> ';
-		html += '<input type="button" value="U" name="U" onclick="insert_text(\'[u]\',\'[/u]\')" /> ';
-		html += '<input type="button" value="URL" name="URL" onclick="insert_text(\'[url]\',\'[/url]\')" /> ';
-		html += '<input type="button" value="IMG" name="IMG" onclick="insert_text(\'[img]\',\'[/img]\')" /> ';
-		html += '<input type="button" value="CODE" name="CODE" onclick="insert_text(\'[code]\',\'[/code]\')" /> ';
-		html += '<input type="button" value="QUOTE" name="QUOTE" onclick="insert_text(\'[quote]\',\'[/quote]\')" />';
-		html += '</div>';
+	}
 
-		msg_field = document.getElementsByName('req_message')[0];
-		label = msg_field.parentNode;
-		fldset = label.parentNode;
-		div = document.createElement('div');
-		div.innerHTML = html;
-		fldset.insertBefore(div, msg_field.parentNode);
-		
-		if (label.getElementsByTagName('strong').length > 0)
-		{
-			label.getElementsByTagName('strong')[0].style.display = 'none';
-			if (label.getElementsByTagName('br').length > 0)
-				label.getElementsByTagName('br')[0].style.display = 'none';
-		}
+	html += '<div>';
+	html += '<input type="button" value="b" name="b" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[b]\',\'[/b]\')" /> ';
+	html += '<input type="button" value="i" name="i" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[i]\',\'[/i]\')" /> ';
+	html += '<input type="button" value="u" name="u" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[u]\',\'[/u]\')" /> ';
+	html += '<input type="button" value="url" name="url" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[url]\',\'[/url]\')" /> ';
+	html += '<input type="button" value="img" name="img" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[img]\',\'[/img]\')" /> ';
+	html += '<input type="button" value="code" name="code" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[code]\',\'[/code]\')" /> ';
+	html += '<input type="button" value="quote" name="quote" onclick="insert_text(this.form.elements[\'' + msgfield + '\'], \'[quote]\',\'[/quote]\')" />';
+	html += '</div>';
 
+	msg_field = document.forms[form].elements[msgfield];
+	label = msg_field.parentNode;
+	if (label.tagName != 'label')
+		label = msg_field
+
+	fldset = label.parentNode;
+	div = document.createElement('div');
+	div.setAttribute('class', 'bbcode-bar');
+	div.setAttribute('style', 'margin-bottom: 4px;');
+	div.innerHTML = html;
+	fldset.insertBefore(div, label);
+
+	if (label.getElementsByTagName('strong').length > 0)
+	{
+		label.getElementsByTagName('strong')[0].style.display = 'none';
+		if (label.getElementsByTagName('br').length > 0)
+			label.getElementsByTagName('br')[0].style.display = 'none';
+	}
+
+	if (!quote_field)
+		quote_field = 'blockpost';
+
+	if (quote_field == 'blockpost')
+	{
 		blockposts = getElementsByClassName('blockpost');
 		for (i in blockposts)
 		{
@@ -178,10 +223,24 @@ addEventListener('load', function (e)
 			quote = getElementsByClassName('postquote', ul)[0];
 			a = quote.getElementsByTagName('a');
 
-			ul.innerHTML += '<li class="postquickquote"><span><a onmousedown="get_quote_text();" onclick="Quote(\'' + id + '\'); return false;" href="' + a.href + '">Quick quote</a></span></li>';
+			ul.innerHTML += '<li class="postquickquote"><span><a onmousedown="get_quote_text();" onclick="Quote(\'' + form + '\', \'' + msgfield + '\', \'' + id + '\'); return false;" href="' + a.href + '">&quot; &quot;</a></span></li>';
 		}
 	}
-}, false);
+	else if (quote_field == 'ticket-history')
+	{
+		blockposts = getElementsByClassName(quote_field);
+		var id = 0;
+
+		for (i in blockposts)
+		{
+			blockposts[i].setAttribute('id', 'p' + id);
+			item_info = getElementsByClassName('time', blockposts[i])[0];
+
+			item_info.innerHTML = '<a onmousedown="get_quote_text();" onclick="Quote(\'' + form + '\', \'' + msgfield + '\', \'' + id + '\'); return false;" href="javascript:;">&quot; &quot;</a> | ' + item_info.innerHTML;
+			id++;
+		}
+	}
+}
 
 function getElementsByClassName(classname, node)
 {
@@ -192,4 +251,19 @@ function getElementsByClassName(classname, node)
 	for (var i=0,j=els.length; i<j; i++)
 		if (re.test(els[i].className)) a.push(els[i]);
 	return a;
+}
+
+window.addEventListener('DOMContentLoaded', bbcode_onload, false);
+
+function bbcode_onload()
+{
+	show_bbcode_bar('quickpostform', 'req_message');
+	show_bbcode_bar('edit', 'req_message');
+	show_bbcode_bar('post', 'req_message');
+
+	if (window.location.href.indexOf('tickets') != -1)
+	{
+		document.forms[0].setAttribute('id', 'ticket-form');
+		show_bbcode_bar('ticket-form', 'comment', 1, 'ticket-history');
+	}
 }
